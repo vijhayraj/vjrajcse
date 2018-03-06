36bda2b15135007e4c6e2845393c5377e7ece2e2
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-06 17:19:36 +0530

    Create prime two.cpp

---
+++ prime two.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class Prime
+{
+    int i,j,a,b,count=0;
+    public:
+    void calculate()
+    {
+        cout<<"enter the number";
+        cin>>a>>b;
+        for(i=a;i<b;i++)
+        {
+            count=0;
+            for(j=2;j<=i/2;j++)
+            {
+            if(i%j==0)
+            {
+              count++;
+              break;
+            }
+            }
+        if(count==0&&i!=1)
+       cout<<"The output is"<<i<<"\t";
+          }
+    }
+
+};
+int main()
+{
+    Prime p;
+    p.calculate();
+   return 0;
+}
