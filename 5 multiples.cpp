5c6268a950a375e630553bd990a1f9b97826cd67
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-06 17:27:06 +0530

    Create 5 multiples.cpp

---
+++ 5 multiples.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,n,a[5];
+    cout<<"enter the value";
+    cin>>n;
+    for(i=1;i<=5;i++)
+    {
+        a[i]=i*n;
+        cout<<a[i]<<endl;
+        }
+}
