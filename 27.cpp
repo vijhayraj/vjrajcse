84c6251e8db62dd03dbf2b53a1296507e82acb12
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-06 17:50:33 +0530

    Create 27.cpp

---
+++ 27.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int num,a,z;
+cout<<"enter the numeric values:";
+cin>>num;
+if(num<a||num>z)
+{
+cout<<"yes,it is numeric";
+}
+else
+{
+cout<<"no,it is not numeric";
+}
+}
