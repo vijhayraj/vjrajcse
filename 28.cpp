279784799bc5421163dd2b03e45b2cc59b8255a2
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-06 17:53:53 +0530

    Create 28.cpp

---
+++ 28.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int  main()
+{
+	int i,a[100],m;
+	cout<<"enetr the number";
+	cin>>n;
+	for(i=0;i<m;i++)
+	{
+	    cin>>a[i];
+	}
+	for(i=0;i<m;i++)
+	{
+	    cout<<a[i]<<"\t"<<i<<"\m";
+	}
+}
