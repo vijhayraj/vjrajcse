457c266ad8bcdbde59a4b36fe0346a2e1f3d55c0
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-06 17:40:01 +0530

    Create 23.cpp

---
+++ 23.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,m,arr[50],large;
+    cout<<"enter the number of elements";
+    cin>>n;
+    cout<<"enter the elements";
+    for(i=0;i<m;++i)
+    {
+          cin>>arr[i];
+    }
+for(i=1;i<m;++i)
+    {
+        if(arr[i]<arr[0])
+                    arr[0]=arr[i];
+        }
+     cout<<"the small value is"<<arr[0];
+    return 0;
+}
