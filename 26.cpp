60db3f44527d859ebd5f2a476c30f030f8b4f0ef
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-06 17:48:31 +0530

    Create 26.cpp

---
+++ 26.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+ int main()
+{
+    int i,m;
+    int sum=0,a[60];
+    int avg=0;
+    cout<<"enter the number";
+    cin>>n;
+    for(i=0;i<m;i++)
+    {
+        cin>>a[i];
+    }
+    for(i=0;i<m;i++)
+    {
+        sum=sum+a[i];
+    }
+avg=sum/m;
+cout<<avg;
+   }
