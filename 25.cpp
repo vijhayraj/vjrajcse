f09e27911661dc50b68afa2df9a894fc7119bc0c
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-06 17:45:36 +0530

    Create 25.cpp

---
+++ 25.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,m,arr[50],temp,med;
+    cout<<"enter the numbeer of elements";
+    cin>>m;
+    cout<<"enter the elements";
+    for(i=0;i<m;++i)
+    {
+          cin>>arr[i];
+    }
+
+    for(i=0;i<m;++i)
+    {
+        for(j=i+1;j<=m;j++)
+        {
+        
+        if(arr[i]>arr[j])
+        {
+            temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+   
+        }
+     }
+         for(i=1;i<=m;++i)
+    {
+     cout<<"the sorted elements are"<<arr[i]<<"\t";
+    }
+    med = arr[(n/2)+1];
+    cout<<"Therefore the median number in the array is:"<<med;
+   return 0;
+}
