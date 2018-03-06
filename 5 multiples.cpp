313b62c14d46504b187ea27541c46b83b69281a8
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-06 17:27:36 +0530

    Update 5 multiples.cpp

--- 5 multiples.cpp
+++ 5 multiples.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
-    int i,n,a[5];
+    int i,n,v[5];
     cout<<"enter the value";
     cin>>n;
     for(i=1;i<=5;i++)
     {
-        a[i]=i*n;
-        cout<<a[i]<<endl;
+        v[i]=i*n;
+        cout<<v[i]<<endl;
         }
 }
