28680b363587cfc656b2a9cb9c867822923bc6b8
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-07 17:53:43 +0530

    Create 52.cpp

---
+++ 52.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+int main()
+{
+
+int n;
+
+cout<<"enter the number";
+
+cin>>n;
+
+switch(n)
+
+{
+case 1:
+cout<<"One";
+break;
+case 2:
+cout<<"Two";
+break;
+case 3:
+cout<<"Three";
+break;
+case 4:
+cout<<"Four";
+break;
+case 5:
+cout<<"Five";
+break;
+case 6:
+cout<<"Six";
+break;
+case 7:
+cout<<"Seven";
+break;
+case 8:
+cout<<"Eight";
+break;
+case 9:
+cout<<"Nine";
+break;
+case 10:
+cout<<"Ten";
+break;
+default:
+cout<<"Type 1_10 only";
+break;
+ }
+ return 0;
+}
