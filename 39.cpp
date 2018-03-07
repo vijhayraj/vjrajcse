40dede53b4307e56d636b5e3d618842ce2eaa7ea
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-07 17:29:06 +0530

    Create 39.cpp

---
+++ 39.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+ 
+using namespace std;
+ int main()
+{
+int a[100],i;
+cout<<"enter the values";
+for(i=0;i<=10;i++)
+{
+cin>>a[i];
+}
+for(i=1;i<=10;i++)
+{
+    if(a[0]<a[i])
+    {
+        a[0]=a[i];
+       }
+    
+    }
+cout<<"the value is"<<a[0];
+}
