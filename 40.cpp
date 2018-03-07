123d99c835318251b9e9cab44431d9df27ab9127
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-07 17:30:47 +0530

    Create 40.cpp

---
+++ 40.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+int i,n, first=0, second=1, next;
+cout<<"Enter number of terms for Series: ";
+cin>>n;
+cout<<"Fibonacci series are: \n";
+for(i=0; i<n; i++)
+{
+cout<<"\n"<<first;
+next = first + second;
+first = second;
+second = next;
+}
+return 0;
+}
