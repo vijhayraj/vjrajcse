dfdafa19e19f813b5dbabe58d2ed8de378ed7aa8
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-08 17:09:26 +0530

    Create 54.cpp

---
+++ 54.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a;
+  cout<<"Enter your number";
+  cin>>a;
+  
+  
+  if(a%2==0)
+  {
+      a=a;
+      cout<<a;
+  }
+ else
+  {
+      a=a-1;
+      cout<<a;
+  }
+
+return 0;
+}
