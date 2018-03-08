6de354c4dade0c5408712631c7f8b078942771d7
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-08 17:15:58 +0530

    Create 55.cpp

---
+++ 55.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  
+  int a,b,n;
+  cout<<"Enter your number";
+  cin>>a>>b;
+  n=a*b;
+
+  if(a%2==0)
+  {
+      cout<<"It is Even";
+}
+else
+
+{
+    cout<<"It is odd";
+}
+
+return 0;
+}
