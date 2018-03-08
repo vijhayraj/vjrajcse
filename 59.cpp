627ffc2794c43cab3c700b81d9c08fb53d7580ad
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-08 17:41:50 +0530

    Update 59.cpp

--- 59.cpp
+++ 59.cpp
@@ -1,33 +1,27 @@
-#include<iostream>
- using namespace std;
- 
+#include <iostream>
+using namespace std;
 
-int main()
-{
-int a[100],i;
-
-cout<<"enter the values";
-
-for(i=0;i<=10;i++)
-
-{
-
-cin>>a[i];
-
-}
-
-for(i=1;i<10;i++)
-
-{
-   
+int main() {
+	
+	int a[10];
+	int i,temp1;
+	
+ for(i=0;i<=10;i++)
+	 {
+	 
+   cin>>a[i];
+		 }
+		for(i=1;i<10;i++)
+	{
+		
    if(a[0]<a[i])
-   
-   {
-       
-       a[0]=a[i];
-        
-    }
-    
-}
-cout<<"the value is"<<a[0];
+		{
+			
+    a[0]=a[i];
+		}
+		
+	}
+	
+ cout<<"maximum element"<<a[0];
+	return 0;
 }
