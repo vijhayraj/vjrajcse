6b24132f6a41f6b5b54e40de1527102f72293954
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-08 17:59:53 +0530

    Create 43.cpp

---
+++ 43.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+  
+  string s;
+   
+   string t;
+  
+  cout<<"Enter the first string :";
+   cin>>s;
+  
+  cout<<"Enter the second string :";
+   cin>>t;
+   
+   cout<<"Concatenation of strings:"<<s<<t;
+     return 0;
+}
