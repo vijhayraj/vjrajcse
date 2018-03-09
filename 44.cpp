aaafdd99d1fa7bdb3dc019903fded51a9133feda
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-09 09:55:07 +0530

    Create 44.cpp

---
+++ 44.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int n;
+ cout<<"Enter the number";
+ cin>>n;
+ if(n>=1&&n<=10)
+ {
+     cout<<"yes number in range";
+ }
+ 
+ else
+ 
+ {
+     cout<<"No,not in range";
+ }
+
+    return 0;
+}
