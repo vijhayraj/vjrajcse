7383a974176520f1dfb73b4466df1a38e65b592d
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-09 17:13:32 +0530

    Create 36.cpp

---
+++ 36.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char a[100];
+	int i,count=0;
+	cout<<"enter the string :";
+    gets(a);
+    
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]<='a'||a[i]>='z')
+		
+		{   count++;
+		    
+		}
+	
+	}
+  
+		cout<<"number of numeric characters in a paragraph :"<<count;
+	return 0;
+}
