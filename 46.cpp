f912335c6406ce7e9e6f1901886d741fdcacd19b
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-09 09:58:22 +0530

    Create 46.cpp

---
+++ 46.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter the number";
+    cin>>a;
+    if(a>=10000000)
+    {
+    cout<<a<<"next number is:"<<a+1;
+}
+
+else
+
+{
+cout<<"number not in range";
+}
+    return 0;
+}
