494acd4ae0f854b526d06b95536965ba1c650076
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-15 17:19:50 +0530

    Create 64.cpp

---
+++ 64.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+
+int main()
+{
+
+    int n,n,n;
+    
+    cout<<"Enter the first number :";
+    cin>>n;
+    
+    cout<<"Enter the second number :";
+    cin>>n;
+    
+    n=n+n;
+    
+    cout<<"the sum of "<<n1<<" "<<"and"<<n2<<"is:"<<n3<<endl;
+    if(n%2==0)
+    
+    {
+        cout<<n<<" is even number ";
+    }
+    
+    else
+    
+    {
+        cout<<n<<" is odd number";
+    }
+ 
+      return 0;
+}
