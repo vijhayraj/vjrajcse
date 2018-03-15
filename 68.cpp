7e71479fbcd7a6c85a96c7d3e0922a1cee63d9f6
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-15 17:41:36 +0530

    Create 68.cpp

---
+++ 68.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+ 
+int main()
+
+{ 
+    int n1,n2,n3;
+    
+    cout<<"Enter the first number :";
+    cin>>n1;
+    
+    if(n1%7==0)
+    
+    {
+        cout<<"yes multiple of seven";
+    }
+    
+    else
+  
+  {
+        cout<<"no ,not multiple of seven";
+    }
+    
+}
