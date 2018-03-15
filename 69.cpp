992d16c2affcc352e17ffb7aeadb0c3dfbd9b9e8
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-15 17:44:03 +0530

    Create 69.cpp

---
+++ 69.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+ 
+int main()
+
+{ 
+   
+   int n1,n2,n3;
+    cout<<"Enter the first number :";
+    cin>>n1;
+    cout<<"enter the second number";
+    cin>>n2;
+    n3=n1-n2;
+    if(n3%2==0)
+   
+   {
+        cout<<"even";
+    }
+    
+    else
+   
+   {
+        cout<<"odd";
+    }
+    
+}
