d126d26e08f30675b7b65646082f499224793210
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-21 17:49:39 +0530

    Create 78.cpp

---
+++ 78.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+int main()
+
+{
+
+  int a,n,i;
+  cout<<"Enter your number";
+  cin>>a;
+  
+ for(i=0;i<=n;i++)
+{
+
+    if(a%13==0)
+    
+    {
+        cout<<"yes";
+    }
+    
+    else
+    
+    {
+        cout<<"no";
+    }
+    
+}
+}
