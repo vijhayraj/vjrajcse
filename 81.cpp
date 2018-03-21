f0137d10070b7f7c4b3ba1ddd8efd9d64771d7d1
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-21 17:56:13 +0530

    Create 81.cpp

---
+++ 81.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+
+    long a,g;
+    while(cin>>a>>g)
+    
+    {
+        cout<<" diffrence between distance:" <<labs(a-g)<<endl;
+        
+    }
+    
+    return 0;
+    
+}
