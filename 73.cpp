cafae9e6bb527b82252c1907dd876f552fbdf6b2
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-21 17:21:18 +0530

    Create 73.cpp

---
+++ 73.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+int main()
+
+{
+
+ int n,a,b;
+ cout<<"enter the number";
+ cin>>n;
+ 
+ cout<<"enter the first number";
+ cin>>a;
+ 
+ cout<<"enter the second number";
+ cin>>b;
+ 
+ if((a<n)&&(b>n))
+ {
+     cout<<"yes";
+ }
+ 
+ else
+ {
+     cout<<"no";
+ }
+
+
+    return 0;
+}
