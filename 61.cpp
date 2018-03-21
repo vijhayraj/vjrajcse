53d8551e6e1f42de3522c9ad6b6f7edc1a996b09
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-21 10:32:29 +0530

    Update 61.cpp

--- 61.cpp
+++ 61.cpp
@@ -2,14 +2,18 @@
 
 using namespace std;
 
-int main()
+int main()0
+    
 {
     string str;
     int i;
+    
     cout<<"enter the string";
     cin>>str;
+    
     cout<<"enter the number";
     cin>>k;
+    
     for(i=0;i<=k;i++)
     {
         cout<<str[i];
