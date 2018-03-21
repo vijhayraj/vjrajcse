5ca4719ee31ef2a2932837da67a037cd6c1488d3
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-21 17:45:57 +0530

    Update 76.cpp

--- 76.cpp
+++ 76.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 using namespace std;
 class Prime
+    
 {
+    
     int i,n,count=0;
     public:
     void calculate()
+        
     {
+        
         cout<<"enter the number";
         cin>>n;
         for(i=2;i<n;i++)
+            
         {
             if(n%i==0)
+                
             {
                 count++;
                 break;
             }
+            
         
         }
+        
     }
     public:
     void display()
@@ -34,6 +42,7 @@ class Prime
     }
 };
 int main()
+    
 {
     Prime p;
     p.calculate();
