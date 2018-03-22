2286cd297ba59b771e4eae994744558883742516
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-22 17:29:02 +0530

    Create 92.cpp

---
+++ 92.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+
+int a,b,i,sum=0;
+
+cout<<"enter the number";
+cin>>a;
+
+for(i=1;i<=a;++i)
+
+{
+    cin>>b;
+	sum=sum+b;
+	
+}
+
+cout<<sum;
+return 0;
+
+}
