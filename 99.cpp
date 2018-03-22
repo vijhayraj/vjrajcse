df4ac917b72c56bad9072f17eff9f18c6b5c4686
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-22 17:43:10 +0530

    Create 99.cpp

---
+++ 99.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+
+        int a,b,c,n;
+        
+        cout<<"enter the number";
+        cin>>a>>b>>c;
+        
+    n=(a*b)%c;
+      cout<<n;
+   
+	return 0;
+  
+}
