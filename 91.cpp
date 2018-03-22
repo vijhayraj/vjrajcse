b6eb089c5841c2b780cf9577f8cda17da4750621
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-22 17:27:10 +0530

    Create 91.cpp

---
+++ 91.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+
+{
+    int L,B,H,V;
+    
+    cout<<"enter the length breadth and height of the cuboid";
+    
+    cin>>L>>B>>H;
+    
+    V=L*B*H;
+    
+    cout<<V;
+}
