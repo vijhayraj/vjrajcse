e8749b061e862cf77209207eedeec75fcdb5e809
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-23 17:27:41 +0530

    Create 102.cpp

---
+++ 102.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main()
+
+{
+    int n;
+    cout<<"Enter the number :";
+    cin>>n;
+    if(n%2==0)
+    
+    {
+    
+        cout<<n/2;
+        
+    }
+    
+    else
+    
+    {
+        cout<<n;
+    }
+}
