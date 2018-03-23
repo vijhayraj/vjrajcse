5f8e6dede7d26739193b61d854239c39f71be742
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-03-23 17:38:20 +0530

    Create 105.cpp

---
+++ 105.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+
+{
+
+ char a;
+ int i;
+ cin>>a;
+ for(i=(int)a-96;i<=26;i++)
+
+    {
+    
+    if((int)a-96==i)
+    
+    {
+    
+    cout<<i;
+    
+    }
+    }
+}
