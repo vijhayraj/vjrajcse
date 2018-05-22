6a6c8d67d3b77bd6d0e867263729b7867555092c
vijhayraj <36921423+vijhayraj@users.noreply.github.com>
2018-05-22 09:50:09 +0530

    Create 112.cpp

---
+++ 112.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+int num,a=0;
+cout<<"Enter any num : ";
+cin>>num;
+while(m>0)
+
+{
+num=num/10;
+a++;
+}
+
+cout<<"\n no. of digits in given number is: "<<a;
+return 0;
+
+}
